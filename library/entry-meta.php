0b6e90c81224206cc620b6841a988146dc10cae9
Nirmalya Ghosh <nirmalya.email@gmail.com>
2015-08-28 00:05:40 +0530

    Small bugfix

--- library/entry-meta.php
+++ library/entry-meta.php
@@ -3,10 +3,10 @@
 if ( ! function_exists( 'magnificient_entry_meta' ) ) {
     
     function magnificient_entry_meta() {
-        echo '<span class="byline author"><a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a> • </span>';
-        echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate><a href="'. get_permalink( ) .'">'. get_the_time('F jS, Y') .'</a> • </time>';
+        echo '<span class="byline author"><a href="'. get_author_posts_url(get_the_author_meta('ID')) .'" rel="author" class="fn">'. get_the_author() .'</a> &#46; </span>';
+        echo '<time class="updated" datetime="'. get_the_time('c') .'" pubdate><a href="'. get_permalink( ) .'">'. get_the_time('F jS, Y') .'</a> &#46; </time>';
         echo '<span class="entry-category">'. the_category( '&bull;' ) .'</span>';
-        echo '<span class="entry-tags">'. the_tags( ' • ', ' • ', '' ) .'</span>';
+        echo '<span class="entry-tags">'. the_tags( ' &#46; ', ' &#46; ', '' ) .'</span>';
     }
     
 }
